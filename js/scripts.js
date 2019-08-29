ccbf3257d89ea6bc04cc9d4c0e2382743a7c7921
Student <example@example.com>
2019-08-29 11:45:13 -0700

    add User Interface Logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,6 @@ function Player(name) {
   this.gameScore = 0,
   this.roundScore = 0
 }
-var newGame = new Game()
-//player clicks roll dice
-//Die returns a random number between 1 and 6
-Player.prototype.rollDie = function(roll) {
-  var roll = Math.floor(Math.random() * 6) + 1;
-  console.log(roll);
-  //player rolls a 1
-  if (roll === 1) {
-    //round score goes to zero
-    this.roundScore = 0;
-    //turn ends
-    newGame.SwitchPlayer();
-  } else {
-    //That number is added to their round score
-    this.roundScore += roll;
-  }
-}
 
 //Game Object
 //2 players
@@ -35,8 +18,25 @@ function Game() {
   console.log(this.currentPlayer);
   //The game has a max score
 }
+
+//player clicks roll dice
+//Die returns a random number between 1 and 6
+Game.prototype.rollDie = function() {
+  var roll = Math.floor(Math.random() * 6) + 1;
+  console.log(roll);
+  //player rolls a 1
+  if (roll === 1) {
+    //round score goes to zero
+    this.currentPlayer.roundScore = 0;
+    //turn ends
+    this.switchPlayer();
+  } else {
+    //That number is added to their round score
+    this.currentPlayer.roundScore += roll;
+  }
+}
 //turn ends
-Game.prototype.SwitchPlayer = function() {
+Game.prototype.switchPlayer = function() {
   if (this.currentPlayer === this.player1) {
   this.currentPlayer = this.player2;
   } else if (this.currentPlayer === this.player2) {
@@ -45,26 +45,29 @@ Game.prototype.SwitchPlayer = function() {
 }
 
 //player can click hold
-Player.prototype.hold = function() {
+Game.prototype.hold = function() {
   //round score is added to total score
-  var store = this.roundScore;
-  this.gameScore += store;
-  this.roundScore = 0;
+  console.log(this);
+  // var storePoints = this.currentPlayer.roundScore;
+  //
+  this.currentPlayer.gameScore += this.currentPlayer.roundScore;
+  this.currentPlayer.roundScore = 0;
   //turn ends
-  if (newGame.currentPlayer.gameScore >= 20){
+  if (this.currentPlayer.gameScore >= 20){
     console.log("You Won!");
-  } else {
-  newGame.SwitchPlayer();
 }
 }
-
 //UI function
 $(document).ready(function(){
-  $(".pigDice#roll").submit(function(event){
-    // var newPlayer = new Player("steve");
-    // var rollDice = Player.rollDie();
-    event.preventDefault();
-
-
+  var newGame = new Game();
+  console.log(newGame);
+  $("#roll").click(function(){
+    newGame.rollDie();
+    $("#rolledScore").text(newGame.currentPlayer.roundScore);
+  });
+  $("#hold").click(function(){
+    newGame.hold();
+    $("#" + newGame.currentPlayer.name).text(newGame.currentPlayer.gameScore);
+    newGame.switchPlayer();
   });
 });
